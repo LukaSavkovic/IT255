844a134365207c08b5a7a14495e6be09e5528419
luka.savkovic.3019 <savkovic97@gmail.com>
2018-02-23 15:10:54 +0100

    Initial commit

---
+++ IT255-DZ01.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title>IT255-DZ01</title>
+</head>
+<body>
+<h1>Luka Savkovic 3019</h1>
+<h2>Ovde ide naslov</h2>
+<p>Ovde ide tekst</p>
+<h3>Ovde ide naslov slike</h3>
+<p>Ako kliknete na sliku prebacice vas na sajt metropolitana</p>
+<a href="http://www.metropolitan.ac.rs/"><img src="https://isum.metropolitan.ac.rs/assets/1.0.1-SNAPSHOT/ctx/layout/img/logometropolitan.png"></a>
+</body>
+</html>
